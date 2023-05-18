6c6a68ee74350976a06f080c0ae8664781406c3c
DanielAros <danielaros200@gmail.com>
2023-05-18 13:26:46 -0600

    Terminada la funcionalidad de encriptacion y descencriptacion para parrafos completos

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import pandas as pd
 import numpy as np
 
 matriz = pd.read_excel("prueba.xlsx")
-mensajeencriptado=[]
 
 columnas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 listafinal = np.vstack([matriz[columna].tolist() for columna in columnas])
 
-# print(listafinal)
 
-textoplano="RESULTA"
+parrafo = "RESULTA QUE EL REY A SUS CINCUENTA Y SIETE ANOS TENIA UN DEFECTILLO BASTANTE MOLESTO NO SE CALLABA NI DEBAJO DEL AGUA YA FUERA DE DIA O DE NOCHE SIEMPRE TENIA ALGO QUE DECIR Y ENLAZABA UNOS TEMAS CON OTROS CON UNA FACILIDAD PASMOSA"
+parrafoEncriptado = "RQGMOXL QGS EX RQM A SGG CUBUXIYTN Y SUSLH AZCK TQBAD UZ DQTWFXTLYC BMGLDREE MAZWVXZ NA SQ CMZDDFL NU DQPSMS DQZ ASIS YM FGSJD DQ DUO O DQ NAQZH SUSESVP TQBAD AXUG QGS DQQAU Y EZZSCEMA UZCK TQASV CAB OFFGV CAB UZO FMQAOMOAQ PMGERWL"
+# textoplano="RESULTA"
 clave="AMOSDELANOCHE"
 
 def encriptar(texto):
+    mensajeencriptado=[]
     for i in range(len(texto)):    
         indicecolumna=list(matriz["A"].tolist()).index(texto[i])
         indicerenglon=list(matriz["A"].tolist()).index(clave[i])
@@ -22,6 +23,7 @@ def encriptar(texto):
     return "".join(mensajeencriptado)
 
 def descencriptar(texto):
+    mensajeencriptado=[]
     for i in range(len(texto)):
         indicecolumna=list(matriz["A"].tolist()).index(clave[i])
         indicerenglon=list(listafinal[indicecolumna]).index(texto[i])
@@ -29,5 +31,26 @@ def descencriptar(texto):
         mensajeencriptado.append(letraencriptada)
     return "".join(mensajeencriptado)
 
+def encriptarDescencriptar(parrafo, metodo):
+    arrayPalabras = parrafo.split()
+    palabras = []
+
+    if(metodo == 'encriptar'):
+        for i in range(len(arrayPalabras)):
+            palabra = encriptar(arrayPalabras[i])
+            palabras.append(palabra)
+        palabrasEncriptadas = " ".join(palabras)
+        print(palabrasEncriptadas)
+    else:
+        for i in range(len(arrayPalabras)):
+            palabra = descencriptar(arrayPalabras[i])
+            palabras.append(palabra)
+        palabrasDescencriptadas = " ".join(palabras)
+        print(palabrasDescencriptadas)
+    
+    
+
 # print(encriptar("RESULTA"))
-print(descencriptar("RQGMOXL"))
+# print(descencriptar("RQGMOXL"))
+# encriptarDescencriptar(parrafo, 'encriptar')
+encriptarDescencriptar(parrafoEncriptado, 'descencriptar')
